7a9e4d3f3d61825f205a220ff147ccb0450a2186
miranda <magui@duck.com>
2022-06-27 15:45:47 -0300

    requisito 8: function fizzBuzz

--- src/challenges.js
+++ src/challenges.js
@@ -39,7 +39,7 @@ function footballPoints(wins, ties) {
 function highestCount(array) {
   let maiorNumero = Math.max.apply(null, array);
   let contador = 0;
-  for (index = 0; index < array.length; index += 1) {
+  for (let index = 0; index < array.length; index += 1) {
     if (array[index] === maiorNumero) {
       contador += 1;
     }
@@ -52,18 +52,30 @@ function catAndMouse(mouse, cat1, cat2) {
   let catOneDistance = Math.abs(mouse - cat1); // utilização do Math.abs() para retornar o valor absoluto no caso da posição ser negativa
   let catTwoDistance = Math.abs(mouse - cat2); // dessa forma, a posição dos valores de posições -x, x na subtração não influenciam o resultado
 
-  if (catOneDistance < catTwoDistance){
-      return 'cat1';
+  if (catOneDistance < catTwoDistance) {
+    return 'cat1';
   } else if (catTwoDistance < catOneDistance) {
-      return 'cat2';
+    return 'cat2';
   } else {
-      return 'os gatos trombam e o rato foge'
+    return 'os gatos trombam e o rato foge';
   }
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let substituir = [];
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] % 3 === 0 && array[index] % 5 === 0) {
+      substituir.push("fizzBuzz");
+    } else if (array[index] % 3 === 0) {
+      substituir.push("fizz");
+    } else if (array[index] % 5 === 0) {
+      substituir.push("buzz");
+    } else {
+      substituir.push("bug!");
+    }
+  }
+  return substituir;
 }
 
 // Desafio 9
