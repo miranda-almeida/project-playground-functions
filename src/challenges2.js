748fb2edd98582dfc8e22da51748e4229a3f7e19
miranda <magui@duck.com>
2022-06-28 16:21:43 -0300

    correções lint referente ao requisito 13  e remoção de código ainda não funcional do requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,21 +1,6 @@
 // Desafio 11
-function generatePhoneNumber(array) {
-  let counter = 0;
-  for (let index = 0; index < array.length; index += 1) {
-  if (array.length < 11 || array.length > 11) {
-  return 'Array com tamanho incorreto.';
-  }
-  for (let repeticoes = index + 1; index < array.length; index += 1) {
-    array[index] === array[repeticoes];
-    counter ++;
-  } 
-  if (array[index] < 0 || array[index] > 9 || counter >= 3) {
-  return 'não é possível gerar um número de telefone com esses valores';
-  } else {
-  let rearange = '(' + array[0] + array[1] + ')' + ' ' + array[2] + array[3] + array[4] + array[5] + array[6] + '-' + array[7] + array[8] + array[9] + array[10];
-  return rearange;
-  }
-}
+function generatePhoneNumber() {
+
 }
 
 // Desafio 12
@@ -37,18 +22,16 @@ function hydrate(string) {
   let numbers = stringNumbers.map(Number); // utiliza o método .map() com a função Number para retornar o array com os números como tipo number, visto que após o .match() os números retornam como tipo string dentro do array.
   let sum = 0;
   for (let index = 0; index < numbers.length; index += 1) {
-  sum += numbers[index]
+    sum += numbers[index];
   }
   let oneWater = ' copo de água';
   let plusWater = ' copos de água';
   if (sum > 1) {
-  return sum + plusWater;
+    return sum + plusWater;
   } else {
-  return sum + oneWater;
+    return sum + oneWater;
   }
 }
-
-
 
 module.exports = {
   generatePhoneNumber,
